bbcb295614911fa34f739db62cb36709f9a1fba3
aaronauk <62733832+aaronauk@users.noreply.github.com>
2022-06-07 16:53:10 -0700

    added submission endpoint

--- api/submissions.js
+++ api/submissions.js
@@ -1,53 +1,21 @@
 const { Router } = require('express');
 const { validateAgainstSchema } = require('../lib/validation');
 const { generateAuthToken, requireAuthentication } = require('../lib/auth');
-const { SubmissionSchema } = require('../models/submission');
+const { SubmissionSchema, getAllSubmissions} = require('../models/submission');
 
 const router = Router();
 
-// router.post('/', async function(req, res, next) {
-//     if (validateAgainstSchema(req.body, UserSchema)) {
-//         const emailTaken = await emailAlreadyUsed(req.body.email)
-//         if (emailTaken) {
-//             res.status(400).send({
-//                 error: "Email already taken"
-//             })
-//         } else {
-//             const id = await insertNewUser(req.body)
-//             res.status(201).send({
-//                 _id: id
-//             })
-//         }
-//     } else {
-//         res.status(400).send({
-//             error: "Request body does not contain a valid User."
-//         })
-//     }
-// })
+router.get('/', async function (req,res,next) {
+    const submissions = await getAllSubmissions();
+    if (submissions.length > 0) {
+      res.status(201).send(submissions);
+    } else {
+      res.status(400).json({
+        error: "There is no submissions to retrieve."
+      });
+    }
+  })
 
-// router.post('/login', async function(req, res) {
-//     if (req.body && req.body.email && req.body.password) {
-//         const user = await getUserByEmail(req.body.email, true)
-//         const authenticated = user && await bcrypt.compare(
-//             req.body.password,
-//             user.password
-//         )
-//         if (authenticated) {
-//             const token = generateAuthToken(user._id, user.role)
-//             res.status(200).send({
-//                 token: token
-//             })
-//         } else {
-//             res.status(401).send({
-//                 error: "Invalid credentials"
-//             })
-//         }
-//     } else {
-//         res.status(400).send({
-//             error: "Request needs email and password."
-//         })
-//     }
-// })
 
 // router.get('/:id', requireAuthentication, async function(req, res, next) {
 //     if (req.role === 'admin' || req.user == req.params.id) {
